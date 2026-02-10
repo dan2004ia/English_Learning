49b7479c029b1639c8296ae57bfd8f5eb4b258b0
dan2004ia <daniasami2004@gmail.com>
2026-02-10 16:10:22 +0200

    Update  file

--- app/src/main/java/com/example/myapplication/fragments/WordOfDayFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/WordOfDayFragment.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
-// استيراد مكتبات Volley للتعامل مع الإنترنت
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
